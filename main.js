2924ead399430a1a16ae729e3a45b3e549bb0d78
Renu Singh <123574550+renu-singhh@users.noreply.github.com>
2023-09-04 20:59:48 +0530

    added

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text" ,{
+    strings: ["Fronted Developer", "Coder", "Backend Developer","Full Stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
